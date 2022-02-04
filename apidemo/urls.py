aab77a40a48efec1c4f2078ddca29c7d3b95088f
Hafizh Aradhana Harimurti <mochikacang1@mail.ugm.ac.id>
2022-02-04 16:10:03 +0700

    Removed admin path

--- apidemo/urls.py
+++ apidemo/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from re import S
-from django.contrib import admin
 from django.urls import path
 
 from apidemo.controllers.visualize_controller import visualize_api
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/', visualize_api.urls)
 ]
